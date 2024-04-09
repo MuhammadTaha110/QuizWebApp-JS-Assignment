3bb26d0b1361f97cd5e7a5b9fe953ca392f13816
MuhammadTaha110 <139653845+MuhammadTaha110@users.noreply.github.com>
2024-04-09 05:08:42 +0500

    Question Added

--- app.js
+++ app.js
@@ -12,3 +12,32 @@ $(window).scroll(function () {
   }
   lastScrollTop = scrollTop;
 });
+
+
+function htmlQuizStart(){
+
+  document.querySelector('.quiz-section').style.display = 'block';
+  document.querySelector('.quiz-cards').style.display= 'none';
+  document.querySelector('.play-quiz').style.display= 'none';
+  document.querySelector('.custom-quiz').style.display= 'none';
+
+}
+
+
+function displayDetails(){
+
+  var section =document.querySelector('.shoe-details');
+  document.querySelector('.shoe-details').style.display= 'flex';
+
+  // Calculate the scroll position
+  var scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+  
+  // Set the top position of the section relative to the scroll position
+  section.style.top = (scrollPosition+30) + 'px';
+
+
+  //document.querySelector('.shoe-cards-section').style.display='none';
+  //document.querySelector('.footer').style.display='none';
+
+}
+
