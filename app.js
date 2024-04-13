1c2c771b571fbea2703891e210a6a6259140afd7
MuhammadTaha110 <139653845+MuhammadTaha110@users.noreply.github.com>
2024-04-13 12:56:46 +0500

    working on Quit Button

--- app.js
+++ app.js
@@ -13,73 +13,73 @@ $(window).scroll(function () {
   lastScrollTop = scrollTop;
 });
 
-var index = 0 ;
+var index = 0;
 const questions = [
-  'HTML Stands for?' ,
-  'Who is making the Web standards?' ,
-  'Choose the correct HTML element for the largest heading:' ,
-  'What is the correct HTML element for inserting a line break?' ,
-  'Choose the correct HTML element to define important text?' 
+  'HTML Stands for?',
+  'Who is making the Web standards?',
+  'Choose the correct HTML element for the largest heading:',
+  'What is the correct HTML element for inserting a line break?',
+  'Choose the correct HTML element to define important text?'
 
 ]
 
 const options = [{
-optionA:'HyperText MakeUp Language',
-optionB:'HyperText MarkUp Language',
-optionC:'HyperTextLink MakeUp Language',
-optionD:'HyperText Website MakeUp Language',
+  optionA: 'HyperText MakeUp Language',
+  optionB: 'HyperText MarkUp Language',
+  optionC: 'HyperTextLink MakeUp Language',
+  optionD: 'HyperText Website MakeUp Language',
 },
 {
-optionA:'Google',
-optionB:'Mozilla',
-optionC:'Safari',
-optionD:'World Wide Web Consortium',
+  optionA: 'Google',
+  optionB: 'Mozilla',
+  optionC: 'Safari',
+  optionD: 'World Wide Web Consortium',
 },
 {
-optionA:'<h1>',
-optionB:'<head>',
-optionC:'<header>',
-optionD:'<h6>',
+  optionA: '<h1>',
+  optionB: '<head>',
+  optionC: '<header>',
+  optionD: '<h6>',
 },
 {
-optionA:'<lineBreak>',
-optionB:'</br>',
-optionC:'<break>',
-optionD:'<lb>',
+  optionA: '<lineBreak>',
+  optionB: '</br>',
+  optionC: '<break>',
+  optionD: '<lb>',
 },
 {
-optionA:'<b>',
-optionB:'<strong>',
-optionC:'<important>',
-optionD:'!important',
+  optionA: '<b>',
+  optionB: '<strong>',
+  optionC: '<important>',
+  optionD: '!important',
 },
 
 ]
 
 const correctOptions = [
-'HyperText Markup Language', 'World Wide Web Consortium' , 
-'<h1>' , '</br>', '<strong>' 
+  'HyperText Markup Language', 'World Wide Web Consortium',
+  '<h1>', '</br>', '<strong>'
 ]
 
-function htmlQuizStart(){
+function htmlQuizStart() {
 
-var Quizsection = document.querySelector('.quiz-section');
-var QuizQuestionNum = document.querySelector('.question-num');
-var QuizQuestion = document.querySelector('.question');
-var A = document.querySelector('.A');
-var B = document.querySelector('.B');
-var C = document.querySelector('.C');
-var D = document.querySelector('.D');
-//var timerMins = document.querySelector('.timer-mins');
-//var timerSec = document.querySelector('.timer-sec');
+  var Quizsection = document.querySelector('.quiz-section');
+  var QuizQuestionNum = document.querySelector('.question-num');
+  var QuizQuestion = document.querySelector('.question');
+  var A = document.querySelector('.A');
+  var B = document.querySelector('.B');
+  var C = document.querySelector('.C');
+  var D = document.querySelector('.D');
+  //var timerMins = document.querySelector('.timer-mins');
+  //var timerSec = document.querySelector('.timer-sec');
 
 
-QuizQuestionNum.innerHTML = index+1;
-QuizQuestion.innerHTML = questions[index];
-A.innerText = options[index].optionA;
-B.innerText = options[index].optionB;
-C.innerText = options[index].optionC;
-D.innerText = options[index].optionD;
+  QuizQuestionNum.innerHTML = index + 1;
+  QuizQuestion.innerHTML = questions[index];
+  A.innerText = options[index].optionA;
+  B.innerText = options[index].optionB;
+  C.innerText = options[index].optionC;
+  D.innerText = options[index].optionD;
 
 
 }
@@ -88,12 +88,12 @@ var timerSeconds = 59;
 var timerMinutes = 14;
 
 
-function timerSec(){
-  if(timerSeconds > 0 ){
+function timerSec() {
+  if (timerSeconds > 0) {
     timerSeconds--;
   }
 
-  else{
+  else {
     timerMinutes--;
     timerSeconds = 59;
   }
@@ -107,24 +107,24 @@ function timerSec(){
 
 htmlQuizStart();
 
-function nextButton(){
-  if(index < questions.length-1){
+function nextButton() {
+  if (index < questions.length - 1) {
     index++;
     htmlQuizStart();
   }
 
 }
 
-function previousButton(){
-  if(index > 0){
+function previousButton() {
+  if (index > 0) {
     index--;
     htmlQuizStart();
   }
-  
+
 }
 
 
-
+let intervalID;
 
 function displayDetails() {
   var section = document.querySelector('.quiz-section');
@@ -140,11 +140,20 @@ function displayDetails() {
 
   // Hide other sections if needed
   document.querySelector('.quiz-cards').style.display = 'none';
-  document.querySelector('.play-quiz').style.display = 'none';
   document.querySelector('.custom-quiz').style.display = 'none';
 
-setInterval(timerSec,1000);
-
-
+  intervalID = setInterval(timerSec, 1000);
 }
+
+function hideDetails() {
+
+  // Display the quiz section
+  document.querySelector('.quiz-section').style.display = 'none';
+
+  document.querySelector('.quiz-cards').style.display = 'flex';
+  document.querySelector('.custom-quiz').style.display = 'block';
+
+  clearInterval(intervalID);
+}
+
 
